f114ed5150d815353e9ed57322e258111f806aab
Moi <profesormoi@gmail.com>
2020-02-01 13:36:08 +0100

    jaja
    jeje
    
    
    Signed-off-by: Moi <profesormoi@gmail.com>

---
+++ prueba3/src/prueba3/programita.java
@@ -0,0 +1,11 @@
+package prueba3;
+
+public class programita {
+
+	public static void main(String[] args) {
+		// TODO Apéndice de método generado automáticamente
+		System.out.println("hey");
+
+	}
+
+}
